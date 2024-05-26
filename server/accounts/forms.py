bd2a2d85c487b443dac74ec12e8e9d847cff34ce
runtee <ekwo.chinonso.01@gmail.com>
2024-05-26 10:20:34 +0100

    fix on the signup and login pages

--- server/accounts/forms.py
+++ server/accounts/forms.py
@@ -11,13 +11,15 @@ from django.contrib.sites.shortcuts import get_current_site
 
 
 class RegistrationForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput)
-    confirm_password = forms.CharField(widget=forms.PasswordInput)
-    country = CountryField(blank_label='(select country)').formfield()
-    phone_number = forms.CharField(max_length=15)
-    terms = forms.BooleanField(required=True, error_messages={'required': 'You must agree to the terms and conditions to register'})
-    referral_code = forms.CharField(required=False)
-
+    confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class': 'form-control account',
+        'placeholder': 'Confirm Password'
+    }))
+    referral_code = forms.CharField(widget= forms.TextInput(attrs={
+                'class': 'form-control account',
+                'placeholder': 'Referral Code'
+            }), required=False)
+    terms = forms.BooleanField()
 
     class Meta:
         model = CustomUser
@@ -65,6 +67,7 @@ class RegistrationForm(forms.ModelForm):
             raise forms.ValidationError("Passwords do not match")
 
         return cleaned_data
+
 
 
 class LoginForm(forms.Form):
--- server/accounts/models.py
+++ server/accounts/models.py
@@ -61,7 +61,6 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
 
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
-    email_verified = models.BooleanField(default=False)
 
     objects = CustomUserManager()
 
