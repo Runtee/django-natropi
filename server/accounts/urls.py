50a17f6c34a4d31e7b6b42f5f0e96fc43622f0c6
Clement Nzubechukwu <clemsnzube@gmail.com>
2024-05-27 20:51:22 +0100

    Fix email template and arrange templates

--- server/accounts/urls.py
+++ server/accounts/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from . import views
 from .views import (
-    CustomPasswordResetView,
     CustomPasswordResetDoneView,
     CustomPasswordResetConfirmView,
     CustomPasswordResetCompleteView
@@ -10,7 +9,7 @@ from .views import (
 urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.login_view, name='login'),
-    path('password_reset/', CustomPasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/', views.custom_password_reset, name='password_reset'),
     path('password_reset/done/', CustomPasswordResetDoneView.as_view(), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', CustomPasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('reset/done/', CustomPasswordResetCompleteView.as_view(), name='password_reset_complete'),
--- server/accounts/views.py
+++ server/accounts/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth import authenticate, login
 from django.contrib.auth.views import PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView, PasswordResetCompleteView
 from .forms import *
 from .models import CustomUser
-from django.urls import reverse_lazy
+from django.urls import reverse, reverse_lazy
 from django.contrib.auth.tokens import default_token_generator
 from django.contrib.sites.shortcuts import get_current_site
 from django.conf import settings
@@ -15,9 +15,14 @@ from django.core.mail import send_mail
 from django.utils.http import urlsafe_base64_decode
 from django.utils.encoding import force_str
 from referral.models import Referral
+from django.utils.html import strip_tags
+from django.core.mail import EmailMultiAlternatives
 from django.contrib.auth import get_user_model
-from django.core.mail import EmailMessage
 from django.contrib import messages
+import logging
+
+logger = logging.getLogger(__name__)
+
 
 
 User = get_user_model()
@@ -45,20 +50,24 @@ def register(request):
 
             # Create email subject and message
             mail_subject = 'Activate your account'
-            message = render_to_string('verification_email.html', {
+            html_message = render_to_string('other/verification_email.html', {
                 'user': user,
                 'domain': current_site.domain,
                 'uid': urlsafe_base64_encode(force_bytes(user.pk)),
                 'token': user.verification_token,
             })
+            plain_message = strip_tags(html_message)
             from_email = settings.DEFAULT_FROM_EMAIL
             to_email = form.cleaned_data.get('email')
 
             # Send the email
-            send_mail(mail_subject, message, from_email, [to_email])
+            email = EmailMultiAlternatives(mail_subject, plain_message, from_email, [to_email])
+            email.attach_alternative(html_message, "text/html")
+            email.send()
+
             messages.success(request, 'Verification email sent. Please check your inbox.')
 
-            return render(request, 'verification_sent.html')
+            return render(request, 'other/verification_sent.html')
         else:
             # If the form is not valid, add an error message
             messages.error(request, 'There was an error with your registration. Please check the details and try again.')
@@ -104,20 +113,36 @@ def login_view(request):
     return render(request, 'login.html', context)
 
 
-class CustomPasswordResetView(PasswordResetView):
-    template_name = 'forgot-password.html'
-    email_template_name = 'password_reset_email.html'
-    subject_template_name = 'password_reset_subject.txt'
-    success_url = reverse_lazy('password_reset_done')
-    form_class = CustomPasswordResetForm
+def custom_password_reset(request):
+    if request.method == 'POST':
+        form = CustomPasswordResetForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data['email']
+            users = CustomUser.objects.filter(email=email, is_active=True)
+            for user in users:
+                uid = urlsafe_base64_encode(force_bytes(user.pk))
+                token = default_token_generator.make_token(user)
+                reset_url = request.build_absolute_uri(
+                    reverse('password_reset_confirm', kwargs={'uidb64': uid, 'token': token})
+                )
+                subject = 'Password Reset Request'
+                message = render_to_string('other/password_reset_email.html', {
+                    'user': user,
+                    'reset_url': reset_url,
+                })
+                send_mail(subject, message, settings.DEFAULT_FROM_EMAIL, [email])
+            return render(request, 'other/password_reset_done.html') 
+    else:
+        form = CustomPasswordResetForm()
+    return render(request, 'forgot-password.html', {'form': form})
 
 class CustomPasswordResetDoneView(PasswordResetDoneView):
-    template_name = 'password_reset_done.html'
+    template_name = 'other/password_reset_done.html'
 
 class CustomPasswordResetConfirmView(PasswordResetConfirmView):
-    template_name = 'password_reset_confirm.html'
+    template_name = 'other/password_reset_confirm.html'
     success_url = reverse_lazy('password_reset_complete')
     form_class = CustomSetPasswordForm
 
 class CustomPasswordResetCompleteView(PasswordResetCompleteView):
-    template_name = 'password_reset_complete.html'
+    template_name = 'other/password_reset_complete.html'
